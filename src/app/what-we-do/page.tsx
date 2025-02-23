c9eadb57757ed8d396af8582e4805713a1a618e8
Varnasathyanath <varnasathyanath.k@gmail.com>
2025-02-23 20:06:31 +0530

    whtWeDoconversion

--- src/app/what-we-do/page.tsx
+++ src/app/what-we-do/page.tsx
@@ -2,14 +2,15 @@ import { NextPage } from "next";
 import Header from "../../components/Header";  // Correct path to Header
 import Footer from "../../components/Footer";  // Correct path to Footer
 import styles from "../../styles/HomePage.module.css";
+import Conversion from "../../components/what-we-do/whatWeDoConversion";  // Correct path to Footer
+
 
 const WhatWeDoPage: NextPage = () => {
   return (
     <div className={styles.homePage}>
       <Header />
       <main>
-        <h1>What We Do</h1>
-        <p>Details about what we do here.</p>
+       <Conversion />
       </main>
       <Footer />
     </div>
---
+++ src/components/what-we-do/whatWeDoConversion.tsx
@@ -0,0 +1,29 @@
+import type { NextPage } from 'next';
+import Image from "next/image";
+import styles from "../../styles/WhatWeDoconversion.module.css";
+
+const HeroFrame:NextPage = () => {
+  	return (
+    		<div className={styles.heroFrame}>
+      			<div className={styles.heroContainer}>
+        				<Image className={styles.scrollDownIcon} width={48} height={55} alt="" src="/Group166.png" />
+        				<div className={styles.heroContainerIn}>
+          					<div className={styles.coverText}>
+            						<div className={styles.coverTextInner}>
+              							<div className={styles.fromConceptToParent}>
+                								<div className={styles.fromConceptTo}>From Concept to</div>
+                								<div className={styles.conversion}>Conversion</div>
+              							</div>
+            						</div>
+          					</div>
+          					<div className={styles.coverImg}>
+            						<Image className={styles.untitled61Icon} width={395} height={275} alt="" src="/whtwedo3.png" />
+            						<Image className={styles.ipadMiniIcon} width={668} height={491} alt="" src="/whtwedo1.png" />
+            						<Image className={styles.untitled51Icon} width={395} height={275} alt="" src="/whtwedo2.png" />
+          					</div>
+        				</div>
+      			</div>
+    		</div>);
+};
+
+export default HeroFrame;
