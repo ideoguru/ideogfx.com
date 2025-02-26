32996765bfb282a13324a3c40ff74cc01529631e
Rohith <rohithjayaraj98@gmail.com>
2025-02-26 14:37:20 +0530

    get in touch page

--- src/app/Get-in-touch/page.tsx
+++ src/app/Get-in-touch/page.tsx
@@ -1,21 +1,23 @@
 // src/app/test/page.tsx (for App Directory routing) or pages/test.tsx (for Pages Directory)
 import { NextPage } from "next";
-import Header from "../../components/Header";  // Correct path to Header
-import Footer from "../../components/Footer";  // Correct path to Footer
+import Header from "../../components/Header"; // Correct path to Header
+import Footer from "../../components/Footer"; // Correct path to Footer
 import styles from "../../styles/HomePage.module.css";
-
+import GetInTouchFrame from "@/components/get-in-touch/GetInTouch_Hero";
 
 const GetIntouchPage: NextPage = () => {
-    return (
-        <div className={styles.homePage}>
-          <Header />
-          <main>
-            <h1>What We Do</h1>
-            <p>Details about what we do here.</p>
-          </main>
-          <Footer />
+  return (
+    <div className={styles.homePage}>
+      <Header />
+      <main>
+        <GetInTouchFrame />
+        <div>
+          <div></div>
         </div>
-      );
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default GetIntouchPage;
